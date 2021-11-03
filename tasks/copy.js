1ac5b3c29f8fe79946e028709209467e6800a0bd
Jeff Linwood <jlinwood@gmail.com>
2021-11-02 20:30:20 -0500

    Upgrade Bootstrap and Sass
    Upgrade for Bootstrap 5 and Gulp Sass 5

--- tasks/copy.js
+++ tasks/copy.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 module.exports = () => {
   return gulp.src([
     'node_modules/jquery/dist/jquery.js',
-    'node_modules/popper.js/dist/popper.js',
+    'node_modules/@popperjs/core/dist/umd/popper.js',
     'node_modules/bootstrap/dist/js/bootstrap.js'
   ])
     .pipe(gulp.dest('./docs/js'))
