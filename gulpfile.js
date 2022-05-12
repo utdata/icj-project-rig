e0e6e9b0725c2fbe833eabc1aacaf4a9c8b7aafd
Mandi Cai <mcai@texastribune.org>
2022-05-12 13:16:03 -0500

    Add updated gulpfile with import statements

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
-const gulp = require("gulp");
-
-// load gulp tasks from ./tasks
-require("gulp-load-tasks")();
+import gulp from 'gulp';
+import { clean } from './tasks/clean.js';
+import { styles } from './tasks/styles.js';
+import { copy } from './tasks/copy.js';
+import { lint } from './tasks/lint.js';
+import { scripts } from './tasks/scripts.js';
+import { images } from './tasks/images.js';
+import { nunjucks } from './tasks/nunjucks.js';
+import { bake } from './tasks/bake.js';
 
 // default tasks
 gulp.task(
-  "default",
+  'default',
   gulp.series(
-    "clean",
-    "styles",
-    "copy",
-    gulp.parallel("lint", "scripts", "images"),
-    "nunjucks",
-    "bake"
+    clean,
+    styles,
+    copy,
+    gulp.parallel(lint, scripts, images)
+    // nunjucks,
+    // bake
   )
 );
 
-gulp.task("dev", gulp.series("default", "serve"));
+gulp.task('dev', gulp.series('default')); // , 'serve'
