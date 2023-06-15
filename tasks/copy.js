c055017b35ff56cd9635e5acaef0e41f68e0a807
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-15 17:13:22 -0500

    Formatting

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,14 +1,15 @@
 import gulp from 'gulp';
 
 function copy() {
-  return gulp.src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/@popperjs/core/dist/umd/popper.js',
-    'node_modules/bootstrap/dist/js/bootstrap.js'
-  ])
-    .pipe(gulp.dest('./docs/js'))
-};
+  return gulp
+    .src([
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/@popperjs/core/dist/umd/popper.js',
+      'node_modules/bootstrap/dist/js/bootstrap.js'
+    ])
+    .pipe(gulp.dest('./docs/js'));
+}
 
-gulp.task('copy', copy)
+gulp.task('copy', copy);
 
 export default copy;
--- tasks/nunjucks.js
+++ tasks/nunjucks.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import nunjucksRender from 'gulp-nunjucks-render';
 import * as journalize from 'journalize';
 import browserSync from 'browser-sync';
-import config from '../project.config.json' assert {type: 'json'};
+import config from '../project.config.json' assert { type: 'json' };
 import log from 'fancy-log';
 import fs from 'fs';
 
@@ -64,15 +64,15 @@ function nunjucks(resolve, reject) {
     .pipe(
       nunjucksRender({
         path: 'src/njk',
-        manageEnv: manageEnv,
+        manageEnv: manageEnv
       })
     )
     .on('error', log.error)
     .pipe(gulp.dest('docs'))
     .pipe(browserSync.stream());
   resolve();
-};
+}
 
-gulp.task('nunjucks', nunjucks)
+gulp.task('nunjucks', nunjucks);
 
 export default nunjucks;
