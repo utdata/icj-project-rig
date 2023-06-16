761837ecb91495a274424324523a99214787f38d
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-16 14:20:25 -0500

    sort imports

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 import gulp from 'gulp';
-import clean from './tasks/clean.js';
-import styles from './tasks/styles.js';
-import copy from './tasks/copy.js';
-import lint from './tasks/lint.js';
-import scripts from './tasks/scripts.js';
-import images from './tasks/images.js';
-import nunjucks from './tasks/nunjucks.js';
 import bake from './tasks/bake.js';
-import serve from './tasks/serve.js';
+import clean from './tasks/clean.js';
 import clear from './tasks/clear.js';
+import copy from './tasks/copy.js';
 import fetch from './tasks/fetch.js';
+import images from './tasks/images.js';
+import lint from './tasks/lint.js';
+import nunjucks from './tasks/nunjucks.js';
+import scripts from './tasks/scripts.js';
+import serve from './tasks/serve.js';
+import styles from './tasks/styles.js';
 
 // default tasks
 gulp.task(
