1214aa8360c537928ad677146220783d27b4e05a
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-17 13:25:34 -0500

    Update format.js

--- tasks/format.js
+++ tasks/format.js
@@ -5,9 +5,10 @@ function format() {
   return gulp
     .src([
       './src/js/*.js',
-      './docs/**/*.html',
-      './docs/*.html',
-      './src/js/**/*.js'
+      './docs/books/*.html',
+      './docs/index.html',
+      './src/js/**/*.js',
+      './src/data/*.json'
     ])
     .pipe(prettier({ config: '.prettierrc.json' }))
     .pipe(gulp.dest((file) => file.base));
