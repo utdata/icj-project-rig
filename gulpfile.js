bdc6421712c73eca40c4899e65001bc901e9acb3
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-17 13:16:40 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ import clear from './tasks/clear.js';
 import copy from './tasks/copy.js';
 import fetch from './tasks/fetch.js';
 import images from './tasks/images.js';
-import lint from './tasks/lint.js';
 import nunjucks from './tasks/nunjucks.js';
 import scripts from './tasks/scripts.js';
 import serve from './tasks/serve.js';
 import styles from './tasks/styles.js';
+import format from './tasks/format.js';
 
 // default tasks
 gulp.task(
@@ -18,9 +18,10 @@ gulp.task(
     clean,
     styles,
     copy,
-    gulp.parallel(lint, scripts, images),
+    gulp.parallel(scripts, images),
     nunjucks,
-    bake
+    bake,
+    format
   )
 );
 
