0b1816048e8d7466d28fae4bf73dcbee92797e77
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-17 14:13:06 -0500

    Update serve.js

--- tasks/serve.js
+++ tasks/serve.js
@@ -28,7 +28,7 @@ function serve() {
   );
   gulp.watch(
     ['src/js/**/*.js'],
-    gulp.series('scripts', 'format', browserSyncReload)
+    gulp.series('format', 'scripts', browserSyncReload)
   );
   gulp.watch(['src/img/**/*'], gulp.series('images', browserSyncReload));
 }
