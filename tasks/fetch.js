7118219d84a9545efe23eb8796ceabd89aef9d23
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-06-23 01:15:08 -0500

    Update fetch.js

--- tasks/fetch.js
+++ tasks/fetch.js
@@ -1,58 +1,73 @@
+import archieML from 'archieml';
+import axios from 'axios';
 import colors from 'ansi-colors';
-import { docToArchieML } from '@newswire/doc-to-archieml';
+import csv from 'csvtojson';
 import fs from 'fs-extra';
-import { google } from 'googleapis';
 import gulp from 'gulp';
 import path from 'path';
-import { sheetToData } from '@newswire/sheet-to-data';
 
 // eslint-disable-next-line no-sync
 const config = fs.readJsonSync('./project.config.json');
 
 async function getData() {
-  const auth = await google.auth.getClient({
-    scopes: [
-      'https://www.googleapis.com/auth/documents.readonly',
-      'https://www.googleapis.com/auth/spreadsheets.readonly'
-    ]
-  });
-
   const { files } = config;
   for (const file of files) {
     const filepath = path.join('src/data', `${file.name}.json`);
 
     let data;
     let color;
+    let response;
+    let url;
 
     switch (file.type) {
       case 'doc':
+        url = `https://docs.google.com/document/d/${file.fileId}/export?format=txt`;
         // eslint-disable-next-line no-await-in-loop
-        data = await docToArchieML({
-          documentId: file.fileId,
-          auth
-        });
+        response = await axios.get(url);
+        console.log(response.data);
         color = 'magenta';
+        data = archieML.load(response.data);
+        console.log(data);
+
+        // eslint-disable-next-line no-await-in-loop
+        await fs.outputJson(filepath, data, {
+          spaces: 2
+        });
+
+        logDownload(file.name, file.fileId, color);
         break;
       case 'sheet':
-        // eslint-disable-next-line no-await-in-loop
-        data = await sheetToData({
-          spreadsheetId: file.fileId,
-          auth
-        });
+        console.log('lol');
         color = 'cyan';
+        url = `https://docs.google.com/spreadsheets/d/${file.fileId}/export?format=csv`;
+        // eslint-disable-next-line no-await-in-loop
+        response = await axios.get(url);
+        console.log(response.data);
+        color = 'magenta';
+
+        data = csv()
+          .fromString(response.data)
+          .then((jsonObj) => {
+            data = jsonObj;
+          })
+          .then(async () => {
+            // eslint-disable-next-line no-await-in-loop
+            await fs.outputJson(
+              filepath,
+              { stores: data },
+              {
+                spaces: 2
+              }
+            );
+
+            logDownload(file.name, file.fileId, color);
+          });
         break;
       default:
         throw new Error(
           `No data fetching method found for type '${file.type}'`
         );
     }
-
-    // eslint-disable-next-line no-await-in-loop
-    await fs.outputJson(filepath, data, {
-      spaces: 2
-    });
-
-    logDownload(file.name, file.fileId, color);
   }
 }
 
